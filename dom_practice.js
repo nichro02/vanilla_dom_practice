a88781968651e73ba8b809c0ba2fc193a1726b92
nichro02 <nichro02@gmail.com>
2020-09-06 15:34:44 -0700

    Ross Nichols-Fatima-store element with id in variable

--- dom_practice.js
+++ dom_practice.js
@@ -1,9 +1,13 @@
 //Test
-console.log('hello world')
+//console.log('hello world')
 
 //Manipulate the DOM
-const h1 = document.querySelector('h1')
-console.log(h1)
+//const h1 = document.querySelector('h1')
+//console.log(h1)
 
-const list = document.querySelectorAll('li')
-console.log(list)
+//const list = document.querySelectorAll('li')
+//console.log(list)
+
+//Assign element with id to variable
+const intro = document.querySelector('#intro')
+console.log(intro)
