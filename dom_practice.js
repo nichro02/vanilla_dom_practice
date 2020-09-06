d7da8084567e64a0695991780e646b9a23bcc179
nichro02 <nichro02@gmail.com>
2020-09-06 15:04:36 -0700

    Test to see if .jpg was added to readme

---
+++ dom_practice.js
@@ -0,0 +1,9 @@
+//Test
+console.log('hello world')
+
+//Manipulate the DOM
+const h1 = document.querySelector('h1')
+console.log(h1)
+
+const list = document.querySelectorAll('li')
+console.log(list)
---
+++ readme.md
@@ -0,0 +1,1 @@
+!(DOM_Tree_Structure.jpg)
